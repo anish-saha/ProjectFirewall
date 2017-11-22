f0bb442f5cd5ed217361afb9cabb9c78fce40225
Anish Saha <asaha@berkeley.edu>
2017-11-22 10:47:16 -0800

    Update common.py

--- common.py
+++ common.py
@@ -193,9 +193,9 @@ class PacketUtils:
     # The second list is T/F 
     # if there is a RST back for that particular request
     def traceroute(self, target, hops):
-        ipList = []
-        rstList = []
-        prev_ips = []
+        ipList = [None] * hops 
+        rstList = [None] * hops
+        prev_ips = [None] * hops
         # Initialization of variables
         source_port = random.randint(2000, 30000)
         payload = "GET /search?q=Falun+Gong HTTP/1.1\nhost: www.google.com\n\n"
